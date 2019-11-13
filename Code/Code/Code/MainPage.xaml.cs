41fa16cd7d57abc2d90615a6db19b06449ae62c4
BrushkouMatvey <brushkoumatvey@gmail.com>
2019-11-14 01:05:42 +0300

    Add FloyItems

--- Code/Code/Code/MainPage.xaml.cs
+++ Code/Code/Code/MainPage.xaml.cs
@@ -11,11 +11,16 @@ namespace Code
     // Learn more about making custom code visible in the Xamarin.Forms previewer
     // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(false)]
-    public partial class MainPage : ContentPage
+    public partial class MainPage : Shell
     {
         public MainPage()
         {
-            InitializeComponent();
+            InitializeComponent(); 
         }
+        private async void MenuItem_Clicked(object sender, System.EventArgs e)
+        {
+            await DisplayAlert("", "Привет Хабр!", "OK");
+        }
+
     }
 }
